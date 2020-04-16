050c421cca602121f0de0d1860358ac4661e4314
Demyx <info@demyx.sh>
2020-04-15 20:41:09 -0700

    Update versions

--- travis.sh
+++ travis.sh
@@ -6,16 +6,16 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # Get versions 
-DEMYX_ALPINE_VERSION="$(docker exec --user=root "$DEMYX_REPOSITORY" cat /etc/os-release | grep VERSION_ID | cut -c 12- | sed -e 's/\r//g')"
-DEMYX_HAPROXY_VERSION="$(docker exec "$DEMYX_REPOSITORY" haproxy -v | awk '{print $3}' | sed -e 's/\r//g')"
+DEMYX_DOCKER_SOCKET_PROXY_ALPINE_VERSION="$(docker exec --user=root "$DEMYX_REPOSITORY" cat /etc/os-release | grep VERSION_ID | cut -c 12- | sed -e 's/\r//g')"
+DEMYX_DOCKER_SOCKET_PROXY_HAPROXY_VERSION="$(docker exec "$DEMYX_REPOSITORY" haproxy -v | awk '{print $3}' | sed -e 's/\r//g')"
 
 # Replace versions
-sed -i "s|alpine-.*.-informational|alpine-${DEMYX_ALPINE_VERSION}-informational|g" README.md
-sed -i "s|haproxy-.*.-informational|haproxy-${DEMYX_HAPROXY_VERSION}-informational|g" README.md
+sed -i "s|alpine-.*.-informational|alpine-${DEMYX_DOCKER_SOCKET_PROXY_ALPINE_VERSION}-informational|g" README.md
+sed -i "s|haproxy-.*.-informational|haproxy-${DEMYX_DOCKER_SOCKET_PROXY_HAPROXY_VERSION}-informational|g" README.md
 
 # Echo versions to file
-echo "DEMYX_ALPINE_VERSION=$DEMYX_ALPINE_VERSION
-DEMYX_HAPROXY_VERSION=$DEMYX_HAPROXY_VERSION" > VERSION
+echo "DEMYX_DOCKER_SOCKET_PROXY_ALPINE_VERSION=$DEMYX_DOCKER_SOCKET_PROXY_ALPINE_VERSION
+DEMYX_DOCKER_SOCKET_PROXY_HAPROXY_VERSION=$DEMYX_DOCKER_SOCKET_PROXY_HAPROXY_VERSION" > VERSION
 
 # Push back to GitHub
 git config --global user.email "travis@travis-ci.org"
@@ -23,7 +23,7 @@ git config --global user.name "Travis CI"
 git remote set-url origin https://${DEMYX_GITHUB_TOKEN}@github.com/demyxco/"$DEMYX_REPOSITORY".git
 # Commit VERSION first
 git add VERSION
-git commit -m "ALPINE $DEMYX_ALPINE_VERSION, HAPROXY $DEMYX_HAPROXY_VERSION"
+git commit -m "ALPINE $DEMYX_DOCKER_SOCKET_PROXY_ALPINE_VERSION, HAPROXY $DEMYX_DOCKER_SOCKET_PROXY_HAPROXY_VERSION"
 git push origin HEAD:master
 # Commit the rest
 git add .
